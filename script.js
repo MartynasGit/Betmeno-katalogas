3e78e7c413b36da6ead712d8bef36e15833f779b
Martynas Gumuliauskas <Mourcius@gmail.com>
2022-07-29 12:38:04 +0300

    init

---
+++ script.js
@@ -0,0 +1,56 @@
+let page = 1;
+let adresas = `http://www.omdbapi.com/?apikey=52cf66a0&s=Batman&page=${page}`
+const filmai = document.querySelector('.filmai')
+const sekantis = document.querySelector('.sekantis')
+const esamas = document.querySelector('.esamas')
+const ankstesnis = document.querySelector('.ankstesnis')
+
+const getData = async (adresas) => {
+    let response = await fetch(adresas);
+    let json = await response.json()
+    return json.Search;
+}
+function patikra(page){
+    if(page == 1 ){
+        ankstesnis.classList.add('btn-secondary')
+        // ankstesnis.classList.add('disabled')
+        ankstesnis.classList.remove('btn-warning');
+        ankstesnis.removeEventListener('click', () => console.log(veikia))
+    }
+    else{
+        ankstesnis.classList.remove('btn-secondary')
+        // ankstesnis.classList.remove('disabled')
+        ankstesnis.classList.add('btn-warning')
+        ankstesnis.addEventListener('click', atgal)
+    }
+}
+function pirmyn() {
+    page++
+    adresas = `http://www.omdbapi.com/?apikey=52cf66a0&s=Batman&page=${page}`
+    esamas.textContent = "Esamas puslapis: " + page;
+    init(adresas);
+}
+function atgal() {
+    page > 1 && page--;
+    adresas = `http://www.omdbapi.com/?apikey=52cf66a0&s=Batman&page=${page}`
+    esamas.textContent = "Esamas puslapis: " + page;
+    init(adresas)
+}
+
+const init = async (adresas) => {
+    patikra(page)
+    let sar =  await getData(adresas)
+    console.log(sar)
+    filmai.innerHTML = ""
+    for (let i = 0; i < 10; i++){
+        filmai.innerHTML += `<div class="text-center my-2">
+                            <img src="${sar[i].Poster}"></img>
+                            <span class="text-center d-block">${sar[i].Title}</span>
+                            <span class="text-center">${sar[i].Year}</span>
+                            <span class="text-center">${sar[i].Type}</span>
+                            <div>`
+    }
+
+}
+init(adresas)
+sekantis.addEventListener('click', pirmyn)
